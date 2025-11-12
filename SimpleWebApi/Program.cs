45c980d5389ca554663e01c80c01a410a5936f76
Lucas Fogliarini Pedroso <lucasfogliarini@gmail.com>
2025-11-12 18:46:01 -0300

    add load endpoint

--- SimpleWebApi/Program.cs
+++ SimpleWebApi/Program.cs
@@ -8,4 +8,25 @@ var app = builder.Build();
 
 app.MapGet("/", () => "API running...");
 
+app.MapGet("/load", async (int tasks = 10, int delayInMs = 1000) =>
+{
+    await RunConcurrentTasksAsync(tasks, delayInMs);
+    return Results.Ok($"{tasks} tarefas executadas simultaneamente com ({delayInMs}ms cada)");
+});
+
 app.Run();
+
+static async Task RunConcurrentTasksAsync(int numberOfTasks, int delayMilliseconds)
+{
+    var tasks = new List<Task>();
+
+    for (int i = 0; i < numberOfTasks; i++)
+    {
+        tasks.Add(Task.Run(async () =>
+        {
+            await Task.Delay(delayMilliseconds);
+        }));
+    }
+
+    await Task.WhenAll(tasks);
+}
